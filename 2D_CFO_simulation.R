6880d976ee94dc34aa6c46bcce547d8c5fc1f9eb
Wenliang WANG <wwangbl@connect.ust.hk>
2022-06-30 00:14:25 +0800

    updates

--- 2D_CFO_simulation.R
+++ 2D_CFO_simulation.R
@@ -13,8 +13,32 @@ p.trues.1 <- list()
 p.trues.1[[1]] <- c(0.05, 0.10, 0.15, 0.30, 0.45)
 p.trues.1[[2]] <- c(0.10, 0.15, 0.30, 0.45, 0.55)
 p.trues.1[[3]] <- c(0.15, 0.30, 0.45, 0.50, 0.60)
-
 p.true.1 <- rbind(p.trues.1[[1]],p.trues.1[[2]],p.trues.1[[3]])
+
+
+p.trues.2 <- list()
+p.trues.2[[1]] <- c(0.15, 0.30, 0.45, 0.50, 0.60)
+p.trues.2[[2]] <- c(0.30, 0.45, 0.50, 0.60, 0.75)
+p.trues.2[[3]] <- c(0.45, 0.55, 0.60, 0.70, 0.80)
+p.true.2 <- rbind(p.trues.2[[1]],p.trues.2[[2]],p.trues.2[[3]])
+
+
+p.trues.3 <- list()
+p.trues.3[[1]] <- c(0.02, 0.07, 0.10, 0.15, 0.30)
+p.trues.3[[2]] <- c(0.7, 0.10, 0.15, 0.30, 0.45)
+p.trues.3[[3]] <- c(0.10, 0.15, 0.30, 0.45, 0.55)
+p.true.3 <- rbind(p.trues.3[[1]],p.trues.3[[2]],p.trues.3[[3]])
+
+
+p.trues.4 <- list()
+p.trues.4[[1]] <- c(0.30, 0.45, 0.60, 0.70, 0.80)
+p.trues.4[[2]] <- c(0.45, 0.55, 0.65, 0.75, 0.85)
+p.trues.4[[3]] <- c(0.50, 0.60, 0.70, 0.80, 0.90)
+p.true.4 <- rbind(p.trues.4[[1]],p.trues.4[[2]],p.trues.4[[3]])
+
+
+
+
 
 # res <- CFO.simu.fn(target, p.true.1, ncohort=ncohort, cohortsize=cohortsize, init.level.A, init.level.B, add.args=add.args)
 # res
@@ -22,8 +46,8 @@ p.true.1 <- rbind(p.trues.1[[1]],p.trues.1[[2]],p.trues.1[[3]])
 n.MTD <- 0
 
 for (i in 1:1000){
-  res <- CFO.simu.fn(target, p.true.1, ncohort=ncohort, cohortsize=cohortsize, init.level.A, init.level.B, add.args=add.args)
-  if (p.true.1[res$MTD[1],res$MTD[2]] == target){
+  res <- CFO.simu.fn(target, p.true.2, ncohort=ncohort, cohortsize=cohortsize, init.level.A, init.level.B, add.args=add.args)
+  if (p.true.2[res$MTD[1],res$MTD[2]] == target){
     n.MTD <- n.MTD + 1
   }
 }
