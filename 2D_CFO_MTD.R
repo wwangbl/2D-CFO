2c62169c0c73c4f5b6de32e174299d31386529db
Wenliang WANG <wwangbl@connect.ust.hk>
2022-07-05 00:18:25 +0800

    updates

--- 2D_CFO_MTD.R
+++ 2D_CFO_MTD.R
@@ -432,3 +432,148 @@ CFO.simu.fn <- function(phi, p.true, ncohort=12, init.level.A=1, init.level.B=1,
 
 
 
+
+select.mtd.comb <- function (target, npts, ntox, cutoff.eli = 0.95, extrasafe = FALSE,
+                             offset = 0.05, boundMTD=FALSE, p.tox=1.4*target,mtd.contour = FALSE)
+{
+  lambda_d = log((1 - target)/(1 - p.tox))/log(p.tox * (1 -target)/(target * (1 - p.tox)))
+  y = ntox
+  n = npts
+  if (nrow(n) > ncol(n) | nrow(y) > ncol(y)) {
+    stop("npts and ntox should be arranged in a way (i.e., rotated) such that for each of them, the number of rows is less than or equal to the number of columns.")
+    
+  }
+  elimi = matrix(0, dim(n)[1], dim(n)[2])
+  if (extrasafe) {
+    if (n[1, 1] >= 3) {
+      if (1 - pbeta(target, y[1, 1] + 1, n[1, 1] - y[1,
+                                                     1] + 1) > cutoff.eli - offset) {
+        elimi[, ] = 1
+      }
+    }
+  }
+  for (i in 1:dim(n)[1]) {
+    for (j in 1:dim(n)[2]) {
+      if (n[i, j] >= 3) {
+        if (1 - pbeta(target, y[i, j] + 1, n[i, j] -
+                      y[i, j] + 1) > cutoff.eli) {
+          elimi[i:dim(n)[1], j] = 1
+          elimi[i, j:dim(n)[2]] = 1
+          break
+        }
+      }
+    }
+  }
+  
+  selectdose=NULL
+  
+  if (elimi[1] == 1) {
+    selectdose = c(99, 99)
+    selectdoses = matrix(selectdose, nrow = 1)
+  }else {
+    phat = (y + 0.05)/(n + 0.1)
+    phat = round(Iso::biviso(phat, n + 0.1, warn = TRUE)[, ],2)
+    # phat.out = phat
+    lower.mat=qbeta(0.025,y+0.05,n-y+0.05)
+    lower.mat=round(Iso::biviso(lower.mat),2)
+    
+    upper.mat=qbeta(0.975,y+0.05,n-y+0.05)
+    upper.mat=round(Iso::biviso(upper.mat),2)
+    phat.out<-matrix(paste0(format(phat,digits=1),"(",lower.mat,", ",upper.mat,")"),byrow=FALSE,nrow=dim(phat)[1])
+    colnames(phat.out)=paste0("B",1:dim(n)[2])
+    rownames(phat.out)=paste0("A",1:dim(n)[1])
+    phat.out.noCI=round(phat,2)
+    phat.out[n == 0] = "NA"
+    phat[elimi == 1] = 1.1
+    phat = phat * (n != 0) + (1e-05) * (matrix(rep(1:dim(n)[1],
+                                                   each = dim(n)[2], len = length(n)), dim(n)[1], byrow = T) +
+                                          matrix(rep(1:dim(n)[2], each = dim(n)[1], len = length(n)),
+                                                 dim(n)[1]))
+    
+    if(boundMTD){
+      if(all(phat[n!=0]>lambda_d)){
+        selectdose = c(99, 99)
+        selectdoses = matrix(selectdose, nrow = 1)
+      }else{
+        phat[phat>lambda_d]=10}}
+    
+    if(is.null(selectdose)){
+      phat[n == 0] = 10
+      selectdose = which(abs(phat - target) == min(abs(phat -
+                                                         target)), arr.ind = TRUE)
+      
+      
+      if (length(selectdose) > 2)
+        selectdose = selectdose[1, ]
+      aa = function(x) as.numeric(as.character(x))
+      if (mtd.contour == TRUE) {
+        selectdoses = cbind(row = 1:dim(n)[1], col = rep(99,
+                                                         dim(n)[1]))
+        for (k in dim(n)[1]:1) {
+          kn = n[k, ]
+          ky = y[k, ]
+          kelimi = elimi[k, ]
+          kphat = phat[k, ]
+          if (kelimi[1] == 1 || sum(n[kelimi == 0]) ==
+              0) {
+            kseldose = 99
+          }else {
+            adm.set = (kn != 0) & (kelimi == 0)
+            adm.index = which(adm.set == T)
+            y.adm = ky[adm.set]
+            n.adm = kn[adm.set]
+            selectd = sort(abs(kphat[adm.set] - target),
+                           index.return = T)$ix[1]
+            kseldose = adm.index[selectd]
+          }
+          selectdoses[k, 2] = ifelse(is.na(kseldose), 99,
+                                     kseldose)
+          if (k < dim(n)[1])
+            if (selectdoses[k + 1, 2] == dim(n)[2])
+              selectdoses[k, 2] = dim(n)[2]
+          if (k < dim(n)[1])
+            if (aa(selectdoses[k + 1, 2]) == dim(n)[2] &
+                aa(selectdoses[k + 1, 2]) == aa(selectdoses[k,
+                                                            2]))
+              selectdoses[k, 2] = 99
+        }
+      }else {
+        selectdoses = matrix(99, nrow = 1, ncol = 2)
+        selectdoses[1, ] = matrix(selectdose, nrow = 1)
+      }
+      
+      selectdoses = matrix(selectdoses[selectdoses[, 2] !=
+                                         99, ], ncol = 2)
+    }
+    
+    colnames(selectdoses) = c("DoseA", "DoseB")
+    
+  }
+  if (mtd.contour == FALSE) {
+    if (selectdoses[1, 1] == 99 && selectdoses[1, 2] == 99) {
+      cat("All tested doses are overly toxic. No MTD is selected! \n")
+     out=list(target = target, MTD = 99, p_est = matrix(NA,nrow = dim(npts)[1], ncol = dim(npts)[2]))
+    }
+    else {
+    
+    out=list(target = target, MTD = selectdoses, p_est=phat.out.noCI,p_est_CI = phat.out)
+    }
+    
+    class(out)<-"boin"
+    return(out)
+  }
+  
+  else {
+    if (length(selectdoses) == 0) {
+      cat("All tested doses are overly toxic. No MTD is selected! \n")
+      out=list(target = target, MTD = 99, p_est = matrix(NA,nrow = dim(npts)[1], ncol = dim(npts)[2]))
+    }
+    else {
+      
+      out=list(target = target, MTD = selectdoses,  p_est=phat.out.noCI,p_est_CI = phat.out)
+    }
+    
+    class(out)<-"boin"
+    return(out)
+  }
+}
